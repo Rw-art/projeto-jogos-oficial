8ec2dbf5bd3f921933fd8a98a0f67381638b285c
amanddot <amanda8mart@gmail.com>
2026-09-15 16:03:43 -0300

    jogos-contador

---
+++ Assets/scripts/cameracontrole.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class cameracontrole : MonoBehaviour
+{
+        public Transform PlayerTransform;
+        private Vector3 PlayerPosition;
+
+    void Start()
+    {
+        PlayerPosition = PlayerTransform.position;
+        transform.position = new Vector3(PlayerPosition.x, PlayerPosition.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerPosition = PlayerTransform.position;
+        transform.position = new Vector3(PlayerPosition.x, PlayerPosition.y, transform.position.z);
+    }
+        
+    }
+
---
+++ Assets/scripts/moeda.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+
+public class TriggerExemplo : MonoBehaviour
+{
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.SendMessage("ChangeTextCoin");
+            Destroy(gameObject);
+                }
+    }
+
+    
+    }
+
